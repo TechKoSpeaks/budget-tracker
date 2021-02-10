2bb76b096d1055f2a790aa0c334cbaaa8922a223
Travis Ko <tkospeaks@gmail.com>
2021-02-10 15:26:37 -0800

    Added functions for reponding in event listener, added manifest icon details

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,18 @@
 
+
+// Constant for all files to cache so they don't have to be written out within function //
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/manifest.webmanifest',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-    '/index.js',
-    '/db.js',
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/index.js',
+  '/db.js',
 ];
 
+
+// Creating constants for cache name and the data for cache name to make comprehension easier
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
@@ -16,44 +20,68 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 // install event handler
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('static').then( cache => {
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-    console.log('Install');
-    self.skipWaiting();
-  });
+  event.waitUntil(
+    caches.open('static').then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  console.log('Install');
+  self.skipWaiting();
+});
 
 
 
-  // activate
-self.addEventListener('activate', function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
-  });
-  
+// activate
+self.addEventListener('activate', function (evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
 
 
 
-  // retrieve assets from cache
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then( response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+
+// retrieve assets from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+
+
+
+  evt.respondWith(
+    caches.open(DATA_CACHE_NAME).then(cache => {
+      return fetch(evt.request)
+        .then(response => {
+          if (response.status === 200) {
+            cache.put(evt.request.url, response.clone());
+          }
+          return response;
+        })
+        .catch(err => {
+          return cache.match(evt.request);
+        });
+    }),
+  );
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    }),
+  );
+});
